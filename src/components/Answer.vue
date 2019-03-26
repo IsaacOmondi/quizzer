1191dcd99e5cfc0edafe51433e5b6a9c437efe12
Isaac Omondi <iomondi90@gmail.com>
2019-03-26 13:04:52 +0300

    add Answer component

---
+++ src/components/Answer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="alert alert-success tex-center">
+        <h3>That's Correct!</h3>
+        <hr>
+        <button class="btn btn-primary" @click="onNextQuestion">Next Question</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        onNextQuestion() {
+            this.$emit('confirmed');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
