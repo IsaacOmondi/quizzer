f4062cca9c70f2d8b60e4368e661874db9f4d0eb
Isaac Omondi <iomondi90@gmail.com>
2019-03-26 13:05:50 +0300

    set up dynamic components for animation of components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+        <h1 class="text-center">The Quizzer</h1>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+        <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Question from './components/Question';
+import Answer from './components/Answer';
+
 
 export default {
-  name: 'app',
+  data() {
+    return{
+      mode: 'app-question'
+    }
+  },
+  methods: {
+    answered(isCorrect) {
+      if (isCorrect) {
+        this.mode = 'app-answer';
+      } else {
+        this.mode = 'app-question';
+        alert('Wrong, try again!');
+      }
+    }
+  },
   components: {
-    HelloWorld
+    appQuestion: Question,
+    appAnswer: Answer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
